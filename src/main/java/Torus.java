f510394258a145b11fbc0cd94d2cec067ea7200e
Tobias Steuer <tst.eu@gmx.de>
2024-12-01 14:24:37 +0100

    Removing comments and refactoring in java class Torus.java

--- src/main/java/Torus.java
+++ src/main/java/Torus.java
@@ -4,65 +4,37 @@ import java.util.List;
 public class Torus {
     private final int size;
     private final Firefly[][] grid;
-
     private List<Double> naturalFrequencies = new ArrayList<>();
 
     public Torus(int size, FireflyClient client) {
         this.size = size;
         this.grid = new Firefly[size][size];
 
-        // Glühwürmchen erstellen
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                double value = lorentzRandom();
-                naturalFrequencies.add(value);
-                grid[i][j] = new Firefly(value, i, j, client);
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                double naturalFrequency = getNaturalFrequency();
+                naturalFrequencies.add(naturalFrequency);
+                grid[i][j] = new Firefly(naturalFrequency, i, j, client);
             }
         }
 
         System.out.printf("Standardabweichung: %.3f%n", calculateStandardDeviation());
         System.out.printf("CupplingStrength / (2 * PI): %.3f%n", Firefly.getCouplingStrength() / (2 * Math.PI));
 
-        // Nachbarn verknüpfen
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
                 Firefly current = grid[i][j];
-                current.addNeighbor(grid[(i - 1 + size) % size][j]); // Oben
-                current.addNeighbor(grid[(i + 1) % size][j]); // Unten
-                current.addNeighbor(grid[i][(j - 1 + size) % size]); // Links
-                current.addNeighbor(grid[i][(j + 1) % size]); // Rechts
+                current.addNeighbor(grid[(i - 1 + size) % size][j]);
+                current.addNeighbor(grid[(i + 1) % size][j]);
+                current.addNeighbor(grid[i][(j - 1 + size) % size]);
+                current.addNeighbor(grid[i][(j + 1) % size]);
             }
         }
     }
 
-    public Firefly[][] getGrid() {
-        return grid;
-    }
+    public Firefly[][] getGrid() { return grid; }
 
-    public double calculateSynchronization() {
-        double realSum = 0;
-        double imagSum = 0;
-        int totalFireflies = size * size;
-
-        for (Firefly[] row : grid) {
-            for (Firefly firefly : row) {
-                double phase = firefly.getPhase() * 2 * Math.PI;
-                realSum += Math.cos(phase);
-                imagSum += Math.sin(phase);
-            }
-        }
-
-        double averageReal = realSum / totalFireflies;
-        double averageImag = imagSum / totalFireflies;
-
-        return Math.sqrt(averageReal * averageReal + averageImag * averageImag);
-    }
-
-    private double lorentzRandom() {
-        double value;
-        value = 0.05 + Math.random() * 0.1;
-        return value;
-    }
+    private double getNaturalFrequency() { return 0.05 + Math.random() * 0.1; }
 
     public double calculateStandardDeviation() {
         double mean = naturalFrequencies.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
