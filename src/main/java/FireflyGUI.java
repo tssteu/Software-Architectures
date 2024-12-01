30122a3f6d1e70f8323375fc88a9285c10ab9753
Tobias Steuer <tst.eu@gmx.de>
2024-12-01 14:28:26 +0100

    Removing comments and refactoring in java class FireflyGUI.java

--- src/main/java/FireflyGUI.java
+++ src/main/java/FireflyGUI.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 public class FireflyGUI extends JFrame {
     private final Torus torus;
-
     public FireflyGUI(Torus torus) {
         this.torus = torus;
         setTitle("Firefly Synchronization");
@@ -16,8 +15,8 @@ public class FireflyGUI extends JFrame {
         Firefly[][] grid = torus.getGrid();
         int cellSize = getWidth() / grid.length;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
                 Firefly firefly = grid[i][j];
                 g.setColor(firefly.isOn() ? Color.GREEN : Color.WHITE);
                 g.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
