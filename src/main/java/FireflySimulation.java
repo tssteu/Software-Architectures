e5e204312d5527245bf63a55ce9f58d092b53155
Tobias Steuer <tst.eu@gmx.de>
2024-12-01 14:11:02 +0100

    Removing comments and refactoring in java class FireflySimulation.java

--- src/main/java/FireflySimulation.java
+++ src/main/java/FireflySimulation.java
@@ -3,42 +3,23 @@ public class FireflySimulation {
         int gridSize = 10;
 
         FireflyClient client = new FireflyClient("localhost", 8080);
-
-        // Torus erstellen und füllen
         Torus torus = new Torus(gridSize, client);
 
-
-        // Firefly-Threads starten
         Firefly[][] grid = torus.getGrid();
-        for (int i = 0; i < gridSize; i++) {
-            for (int j = 0; j < gridSize; j++) {
+        for(int i = 0; i < gridSize; i++) {
+            for(int j = 0; j < gridSize; j++) {
                 new Thread(grid[i][j]).start();
             }
         }
 
-        // GUI starten
         FireflyGUI gui = new FireflyGUI(torus);
         gui.setVisible(true);
 
-        // Animation-Loop mit Synchronisationsausgabe
-        new Thread(() -> {
-            while (true) {
-                double synchronization = torus.calculateSynchronization();
-                System.out.printf("Synchronisation: R = %.3f%n", synchronization);
-                try {
-                    Thread.sleep(1000); // Ausgabe alle 1000 ms
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }).start();
-
-        // GUI-Aktualisierung
-        while (true) {
+        while(true) {
             gui.repaint();
             try {
-                Thread.sleep(100); // Refresh alle 100ms
-            } catch (InterruptedException e) {
+                Thread.sleep(100);
+            } catch(InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
